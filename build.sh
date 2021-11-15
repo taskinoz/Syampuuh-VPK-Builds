469bc6b1456d7f3780f939a68d39fb1ab8be1a7a
Tristan Kennedy <tristan@taskinoz.com>
2021-11-15 21:59:03 +1100

    Added loop that repacks directories

--- build.sh
+++ build.sh
@@ -4,3 +4,17 @@
 ## https://github.com/mrsteyk/RSPNVPK
 ## Add the VPK that your editing in a /dir folder
 ## eg. englishclient_frontend.bsp.pak000_dir.vpk
+
+dirs=("Brutal-Titanfall" "Brutal-Titanfall-2" "Englist-Text-Japan-Subs" "Improved-Crosshairs" "Improved-Sunflares" "No-HUD" "Photo-Mode" "Pilot-Speedometer")
+vpks=("frontend" "frontend" "frontend" "mp_common" "frontend" "mp_common" "mp_common" "mp_common")
+
+for i in 0 1 2 3 4 5 6 7
+do
+   echo "Mod: ${dirs[$i]}"
+   echo "VPK: ${vpks[$i]}"
+
+   mkdir "export/${dirs[$i]}-export"
+   cp "./dir/englishclient_${vpks[$i]}.bsp.pak000_dir.vpk" ./
+   RSPNVPK "englishclient_${vpks[$i]}.bsp.pak000_dir.vpk" -d ${dirs[$i]} -s
+   mv ./*.vpk "export/${dirs[$i]}-export"
+done
