6d0f8627bf87de54405d07431e3c8c327628dd34
Tristan Kennedy <tristan@taskinoz.com>
2021-11-15 21:16:52 +1100

    Added base build file

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## This is an auto build script that can be used with the new RSPNVPK tool
+## https://github.com/mrsteyk/RSPNVPK
+## Add the VPK that your editing in a /dir folder
+## eg. englishclient_frontend.bsp.pak000_dir.vpk
