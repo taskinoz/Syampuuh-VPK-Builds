6484a0a48dbc1a85160cd812034fedcf1fed0108
Tristan Kennedy <tristan@taskinoz.com>
2021-11-27 23:03:19 +1100

    added zip parameter

--- build.sh
+++ build.sh
@@ -8,13 +8,15 @@
 dirs=("Brutal-Titanfall" "Brutal-Titanfall-2" "Englist-Text-Japan-Subs" "Improved-Crosshairs" "Improved-Sunflares" "No-HUD" "Photo-Mode" "Pilot-Speedometer")
 vpks=("frontend" "frontend" "frontend" "mp_common" "frontend" "mp_common" "mp_common" "mp_common")
 
+mkdir "export"
+
 for i in 0 1 2 3 4 5 6 7
 do
    echo "Mod: ${dirs[$i]}"
    echo "VPK: ${vpks[$i]}"
 
-   mkdir "export/${dirs[$i]}-export"
+   mkdir "export/${dirs[$i]}"
    cp "./dir/englishclient_${vpks[$i]}.bsp.pak000_dir.vpk" ./
    RSPNVPK "englishclient_${vpks[$i]}.bsp.pak000_dir.vpk" -d ${dirs[$i]} -s
-   mv ./*.vpk "export/${dirs[$i]}-export"
+   mv ./*.vpk "export/${dirs[$i]}"
 done
